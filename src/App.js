757dee63b848b4bc2af8448025dcac2a1031d619
Jonathan <jonathantilling89@gmail.com>
2020-04-06 23:30:18 +0200

    add links to menu-items as proper url slugs

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
 import React from "react";
 import "./App.css";
 
+//Switch stoppar koden från att läsas efter den har fått en träff innanför <switch />
+import { Switch, Route } from "react-router-dom";
+
 import HomePage from "./pages/homepage/homepage.component";
+
+const HatsPage = () => (
+  <div>
+    <h1>HATS PAGE</h1>
+  </div>
+);
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route exact={true} path="/hats" component={HatsPage} />
+      </Switch>
+      {/* <HomePage /> */}
     </div>
   );
 }
